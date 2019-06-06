fd10c1c1a691febdacce20e4e4e5176eaff590cb
Sam V <36467696+PinkGerbil@users.noreply.github.com>
2019-06-07 09:33:45 +1000

    -Committing scene Changes and script edits

--- Unity/VRGameJam/Assets/Scripts/OVRController.cs
+++ Unity/VRGameJam/Assets/Scripts/OVRController.cs
@@ -4,17 +4,70 @@ using UnityEngine;
 
 public class OVRController : MonoBehaviour
 {
-    OVRControllerHelper
-    
+    public float distance = 10.0f;
+    public LineRenderer lineRenderer = null;
+    public LayerMask everythingMask = 0;
+    public LayerMask interactableMask = 0;
+
+    private Transform CurrentOrigin; 
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        SetLineColour();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 hitPoint = UpdateLine();
+    }
+
+    private Vector3 UpdateLine()
+    {
+        RaycastHit hit = CreateRaycast(everythingMask);
+        Vector3 endPosition = CurrentOrigin.position + (CurrentOrigin.forward * distance);
+        if(hit.collider != null)
+        {
+            endPosition = hit.point; 
+        }
+
+        lineRenderer.SetPosition(0, CurrentOrigin.position);
+        lineRenderer.SetPosition(1, endPosition);
+
+        return endPosition;
+    }
+
+    private void UpdateOrigin(OVRInput.Controller controller, GameObject controllerObject)
+    {
+        CurrentOrigin = controllerObject.transform;
+
+        if(controller == OVRInput.Controller.Touchpad)
+        {
+            lineRenderer.enabled = false;
+        }
+        else
+        {
+            lineRenderer.enabled = true; 
+        }
+    }
+
+    private RaycastHit CreateRaycast(int layer)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(CurrentOrigin.position, CurrentOrigin.forward);
+        Physics.Raycast(ray, out hit, distance, layer);
+
+        return hit;
+    }
+
+    private void SetLineColour()
+    {
+        if (!lineRenderer)
+        {
+            return; 
+        }
+        Color endColour = Color.white;
+        lineRenderer.endColor = endColour;
     }
 }
